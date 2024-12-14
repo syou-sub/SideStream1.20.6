50bce46abc8c13417d951eca9acc01c4121eae58
Jille342 <151497575+Jille342@users.noreply.github.com>
2024-12-14 11:39:09 +0900

    a lot of changes

--- src/main/java/client/features/modules/render/NameTags.java
+++ src/main/java/client/features/modules/render/NameTags.java
@@ -5,13 +5,17 @@ import client.event.listeners.EventNameTag;
 import client.event.listeners.EventRender2D;
 import client.event.listeners.EventRender3D;
 import client.features.modules.Module;
+import client.settings.BooleanSetting;
 import client.settings.NumberSetting;
+import client.utils.RenderingUtils;
 import client.utils.font.TTFFontRenderer;
 import java.awt.Color;
 import java.util.Comparator;
 import java.util.Objects;
 
 import me.x150.renderer.render.MSAAFramebuffer;
+import me.x150.renderer.render.Renderer2d;
+import me.x150.renderer.util.RendererUtils;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.render.Camera;
@@ -39,21 +43,25 @@ public class NameTags extends Module
 	private static final int MAX_SAMPLES =
 		GL30.glGetInteger(GL30C.GL_MAX_SAMPLES);
 	private final TTFFontRenderer font = TTFFontRenderer.of("ElliotSans", 8);
-	private  TTFFontRenderer nameDrawer;
-		public NumberSetting size;
+	private final TTFFontRenderer nameDrawer = TTFFontRenderer.of("ElliotSans", 12);
+		public static NumberSetting size;
+
+		public BooleanSetting armor;
 	
 	public NameTags()
 	{
 		super("NameTags", 0, Category.RENDER);
-		size = new NumberSetting("Size", 10, 8, 20,1);
-		addSetting(size);
+		size = new NumberSetting("Size", 1.0, 1.0, 5.0,0.1);
+		armor = new BooleanSetting("Armor", true);
+		addSetting(size,armor);
 	}
+
 	
 	public void onEvent(Event<?> event)
 	{
-		if(event instanceof EventRender3D)
+		if(event instanceof EventRender2D)
 		{
-			nameDrawer = TTFFontRenderer.of("ElliotSans", (int) size.getValue());
+			;
 			if(mc.player == null || mc.world == null)
 				return;
 			if(mc.gameRenderer.getCamera() == null)
@@ -68,7 +76,7 @@ public class NameTags extends Module
 							.equals(mc.player))
 					.toList())
 				{
-					this.render(((EventRender3D)event).getMatrix(), player,
+					this.render(((EventRender2D) event), player,
 						player.getName());
 				}
 			});
@@ -76,9 +84,10 @@ public class NameTags extends Module
 		
 	}
 	
-	public void render(final MatrixStack stack,
+	public void render(EventRender2D event,
 		final AbstractClientPlayerEntity entity, final Text text)
 	{
+		MatrixStack stack = event.getContext().getMatrices();
 		final String t = text.getString();
 		
 		final Vec3d headPos = getInterpolatedEntityPosition(entity).add(0,
@@ -86,8 +95,9 @@ public class NameTags extends Module
 		final Vec3d a = getScreenSpaceCoordinate(headPos, stack);
 		if(isOnScreen(a))
 		{
-			TickManager.runOnNextRender(
-				(event) -> this.drawInternal(event, a, t, entity));
+		//	TickManager.runOnNextRender(
+		//		(event) -> this.drawInternal(event, a, t, entity));
+			drawInternal(event, a,t, entity);
 		}
 	}
 	
@@ -109,21 +119,17 @@ public class NameTags extends Module
 		final int displayHeight = mc.getWindow().getHeight();
 		final int[] viewport = new int[4];
 		final Vector3f target = new Vector3f();
-		
-		final double deltaX = pos.x - camera.getPos().x;
-		final double deltaY = pos.y - camera.getPos().y;
-		final double deltaZ = pos.z - camera.getPos().z;
-		
+        double deltaX = pos.x - camera.getPos().x;
+		 double deltaY = pos.y - camera.getPos().y;
+		 double deltaZ = pos.z - camera.getPos().z;
+
 		GL11.glGetIntegerv(GL11.GL_VIEWPORT, viewport);
 		
 		final Vector4f transformedCoordinates =
 			new Vector4f((float)deltaX, (float)deltaY, (float)deltaZ, 1.f)
 				.mul(lastWorldSpaceMatrix);
-		
-		final Matrix4f matrixProj = new Matrix4f(lastProjMat);
-		final Matrix4f matrixModel = new Matrix4f(lastModMat);
-		
-		matrixProj.mul(matrixModel).project(transformedCoordinates.x(),
+
+        lastProjMat.mul(lastModMat).project(transformedCoordinates.x(),
 			transformedCoordinates.y(), transformedCoordinates.z(), viewport,
 			target);
 		
@@ -138,76 +144,55 @@ public class NameTags extends Module
 	}
 	
 	void drawInternal(final EventRender2D eve, final Vec3d screenPos,
-		final String text, final AbstractClientPlayerEntity entity)
-	{
-		final double healthHeight = 2;
-		final double labelHeight = 2 + this.nameDrawer.getFontHeight();
+		final String text, final AbstractClientPlayerEntity entity) {
+
+		final double labelHeight = 2 + nameDrawer.getFontHeight();
 		int ping = -1;
-		GameMode gamemode = null;
-		final PlayerListEntry ple =
-			Objects.requireNonNull(mc.getNetworkHandler())
-				.getPlayerListEntry(entity.getUuid());
-		if(ple != null)
-		{
-			gamemode = ple.getGameMode();
+		final PlayerListEntry ple = Objects.requireNonNull(mc.getNetworkHandler()).getPlayerListEntry(entity.getUuid());
+		if (ple != null) {
 			ping = ple.getLatency();
 		}
+
 		final String pingStr = (ping == 0 ? "?" : ping) + " ms";
 		final MatrixStack stack1 = eve.getContext().getMatrices();
+		stack1.push();
 		final Vec3d actual =
-			new Vec3d(screenPos.x, screenPos.y - labelHeight, screenPos.z);
-		float width = this.nameDrawer.getStringWidth(text) + 4;
-		Color color = null;
-		color = (entity.isSneaking() || entity.isInvisible())
-			? new Color(100, 0, 0, 100) : new Color(0, 0, 5, 100);
-		
-		me.x150.renderer.render.Renderer2d.renderQuad(stack1, color,
-			actual.x - width / 2d, actual.y, actual.x + width / 2d,
-			actual.y + labelHeight);
-		this.nameDrawer.drawString(stack1, text,
-			actual.x + width / 2d - this.nameDrawer.getStringWidth(text) - 2,
-			actual.y + 2, entity.getTeamColorValue());
-		if(ping != -1)
-		{
+				new Vec3d(screenPos.x, screenPos.y - labelHeight, screenPos.z);
+		float width = nameDrawer.getStringWidth(text) + 4;
+		Color color = (entity.isSneaking() || entity.isInvisible())
+				? new Color(100, 0, 0, 100) : new Color(0, 0, 5, 100);
+
+	RenderingUtils.renderRect(stack1, actual.x - width / 2d, actual.y, actual.x + width / 2d, actual.y + labelHeight, color.getRGB());
+		nameDrawer.drawString(stack1, text,
+				actual.x + width / 2d - nameDrawer.getStringWidth(text) - 2,
+				actual.y + 2, entity.getTeamColorValue());
+		if (ping != -1) {
 			this.font.drawString(stack1, pingStr, actual.x - width / 2d + 2,
-				actual.y + 2 + this.nameDrawer.getFontHeight(), 0xAAAAAA);
+					actual.y + 2 + nameDrawer.getFontHeight(), 0xAAAAAA);
 		}
-		// me.x150.renderer.render.Renderer2d.renderRoundedQuad(stack1, new
-		// Color(60, 60, 60, 255), actual.x - width / 2d + 2, actual.y +
-		// labelHeight - 2 - healthHeight, actual.x + width / 2d - 2, actual.y +
-		// labelHeight - 2, (float) (healthHeight / 2d), 10);
-		final float health = entity.getHealth();
-		final float maxHealth = entity.getMaxHealth();
-		float healthPer = health / maxHealth;
-		healthPer = MathHelper.clamp(healthPer, 0, 1);
-		final double drawTo =
-			MathHelper.lerp(healthPer, actual.x - width / 2d + 2 + healthHeight,
-				actual.x + width / 2d - 2);
-		final Color MID_END = me.x150.renderer.util.RendererUtils
-			.lerp(Color.GREEN, Color.RED, healthPer);
-		// me.x150.renderer.render.Renderer2d.renderRoundedQuad(stack1, MID_END,
-		// actual.x - width / 2d + 2, actual.y + labelHeight - 2 - healthHeight,
-		// drawTo, actual.y + labelHeight - 2, (float) (healthHeight / 2d), 10);
-		float xOffset = 0f;
-		for(final ItemStack stack : entity.getInventory().armor)
-		{
-			if(stack.getItem() != Items.AIR)
-			{
-				eve.getContext().drawItem(stack,
-					(int)(actual.x - width / 2d + 2
-						+ font.getStringWidth(pingStr) + xOffset),
-					(int)(actual.y - 10));
+		if (armor.getValue()) {
+			float xOffset = 0f;
+			for (final ItemStack stack : entity.getInventory().armor) {
+				if (stack.getItem() != Items.AIR) {
+					eve.getContext().drawItem(stack,
+							(int) (actual.x - width / 2d + 2
+									+ font.getStringWidth(pingStr) + xOffset),
+							(int) (actual.y - 10));
+				}
+				xOffset += 20;
 			}
-			xOffset += 20;
+
+			eve.getContext().drawItem(entity.getInventory().getMainHandStack(),
+					(int) (actual.x - width / 2d + 2 - 10), (int) (actual.y - 10));
 		}
-		try
-		{
-			eve.getContext().drawItem(entity.getHandItems().iterator().next(),
-				(int)(actual.x - width / 2d + 2 - 10), (int)(actual.y - 10));
-		}catch(final Exception ignored)
-		{}
+
+		stack1.pop();
 	}
+	/*
+
 	public void onNameTag(EventNameTag eventNameTag){
 		eventNameTag.cancel();
 	}
+
+	 */
 }
