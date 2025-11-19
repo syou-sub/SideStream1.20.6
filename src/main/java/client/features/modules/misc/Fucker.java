6e306d428b11f887753c43edd8646c3126436e5c
Null <andounatumi.xxxxxx@gmail.com>
2025-11-19 16:55:10 +0900

    first commit

--- src/main/java/client/features/modules/misc/Fucker.java
+++ src/main/java/client/features/modules/misc/Fucker.java
@@ -19,58 +19,50 @@ import net.minecraft.util.math.Vec3d;
 /**
  * Created by cool1 on 1/19/2017.
  */
-public class Fucker extends Module
-{
+public class Fucker extends Module {
 
     public static BlockPos blockBreaking;
     float[] rotations = null;
-    private  BooleanSetting tp;
-    private  BooleanSetting suiren;
-    private  NumberSetting range;
+    private BooleanSetting tp;
+    private BooleanSetting suiren;
+    private NumberSetting range;
 
 
-
-    public Fucker()
-    {
+    public Fucker() {
         super("Fucker", 0, Category.MISC);
     }
 
     @Override
-    public void onDisabled()
-    {
+    public void onDisabled() {
         blockBreaking = null;
         super.onDisabled();
     }
 
-    public void init()
-    {
-        tp = new BooleanSetting("TP ",true);
+    public void init() {
+        tp = new BooleanSetting("TP ", true);
         suiren = new BooleanSetting("Suiren", false);
-        range = new NumberSetting("Range ", 6.0 , 6.0, 7.0 ,0.1 );
-super.init();
-addSetting(tp,suiren, range);
+        range = new NumberSetting("Range ", 6.0, 6.0, 7.0, 0.1);
+        super.init();
+        addSetting(tp, suiren, range);
     }
 
     @Override
-    public void onEvent(Event<?> event)
-    {
-        if(event instanceof EventUpdate)
-        {
+    public void onEvent(Event<?> event) {
+        if (event instanceof EventUpdate) {
             setTag(String.valueOf(range.getValue()));
             RaycastUtils raycastUtils = new RaycastUtils();
             if (mc.player.age % 10 == 0) {
-                int range = (int)this.range.getValue();
-
-                for(int x = -range; x <= range; ++x) {
-                    for(int y = -range; y <= range; ++y) {
-                        for(int z = -range; z <= range; ++z) {
+                int range = (int) this.range.getValue();
+                for (int x = -range; x <= range; ++x) {
+                    for (int y = -range; y <= range; ++y) {
+                        for (int z = -range; z <= range; ++z) {
                             BlockPos tpPos = mc.player.getBlockPos().add(x, y, z);
                             Block block = mc.world.getBlockState(tpPos).getBlock();
                             if (tp.getValue() && block == Blocks.NETHER_QUARTZ_ORE) {
-                              HitResult  hitResult = raycastUtils.rayCast(RotationUtils.getAngleToBlockPos(tpPos),Math.sqrt(tpPos.getSquaredDistance(mc.player.getEyePos())), mc.getTickDelta());
-                                Direction facing = ((BlockHitResult)hitResult).getSide();
-                                mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(mc.player.getBlockPos().toCenterPos(), facing, tpPos, false));
-
+                                HitResult hitResult = raycastUtils.rayCast(RotationUtils.getAngleToBlockPos(tpPos), Math.sqrt(tpPos.getSquaredDistance(mc.player.getEyePos())), mc.getTickDelta());
+                                Direction facing = ((BlockHitResult) hitResult).getSide();
+                                //    mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(mc.player.getBlockPos().toCenterPos(), facing, tpPos, false));
+                                mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, new BlockHitResult(mc.player.getBlockPos().toCenterPos(), null, tpPos, false));
                             }
 
                             boolean shouldBreak = suiren.getValue() && block == Blocks.LILY_PAD;
@@ -84,8 +76,6 @@ addSetting(tp,suiren, range);
 
             }
         }
-
-
     }
-    }
+}
 
